a25e5b84e5f47a22f1d5d93182fe74046a008399
nicolasances <nicolas.matteazzi@gmail.com>
2020-01-01 18:31:32 +0100

    Added GET /foods/{id}

---
+++ dlg/GetFood.js
@@ -0,0 +1,33 @@
+var mongo = require('mongodb');
+var config = require('../config');
+var converter = require('../conv/FoodConverter');
+
+var MongoClient = mongo.MongoClient;
+
+exports.do = function(req) {
+
+  var foodId = req.params.id;
+
+  return new Promise(function(success, failure) {
+
+    return MongoClient.connect(config.mongoUrl, function(err, db) {
+
+      var mongoFilter = {};
+      if (filter.category != null) mongoFilter = {category: filter.category};
+
+      var results = db.db(config.dbName).collection(config.collections.food).find({_id: new mongo.ObjectId(foodId)}).toArray(function(err, array) {
+
+        db.close();
+
+        if (array.length == 0) {
+          success({});
+          return;
+        }
+
+        success(converter.foodConverter.toFood(array[0]));
+
+      });
+    });
+  });
+
+}
